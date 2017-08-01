4dc785a28ce075570d3075c5ed4b097029018d88
Edith Mandujano <edithmandujano001@gmail.com>
2017-08-01 13:13:58 -0500

    revision handlebar

--- dist/assets/js/compilar_index.js
+++ dist/assets/js/compilar_index.js
@@ -1,20 +1,22 @@
-var template=Handlebars.js['plantilla_index'];
-var data={
-    articulos=[
-      {
-      titulo:"Titulo1",
-      parrafo:"parrafo1"
-    },
+var template = Handlebars.tamplates['index'];
+var data = {
+	articulos: [
     {
-      titulo:"Titulo1",
-      texto:"parrafo1"
-    },
-    {
-      titulo:"Titulo2",
-      texto:"parrafo2"
-    },
-    {
-      titulo:"Titulo3",
-      texto:"parrafo3"
-    };
-document.getElementById('articulos').innerHTML+=template(data);
+			titulo: "Titulo1",
+			parrafo: "parrafo1"
+		},
+		{
+			titulo: "Titulo1",
+			texto: "parrafo1"
+		},
+		{
+			titulo: "Titulo2",
+			texto: "parrafo2"
+		},
+		{
+			titulo: "Titulo3",
+			texto: "parrafo3"
+		}
+	]
+}
+document.getElementById("handle").innerHTML += template(articulos);
